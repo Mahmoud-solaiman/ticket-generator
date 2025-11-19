09a2588396148b3017d362b04aa48e2866b39576
Mahmoud Solaiman <mahmoudalshahat420@gmail.com>
2025-11-19 09:04:28 +0200

    Make the drag and drop feature work

--- src/pages/home/UploadArea.jsx
+++ src/pages/home/UploadArea.jsx
@@ -5,7 +5,8 @@ import './UploadArea.css';
 
 export function UploadArea({ setImage }) {
   const [isBigger, setIsBigger] = useState(false);
-  const [ isUploaded, setIsUploaded ] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [isDragged, setIsDragged] = useState(false);
   const inputElem = useRef(null);
   const uploadedImage = useRef(null);
 
@@ -13,12 +14,53 @@ export function UploadArea({ setImage }) {
     inputElem.current.click();
   }
 
+  function uploadImage(imageSize, image) {
+    if (imageSize / 1024 <= 500) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        uploadedImage.current.src = event.target.result;
+        setImage(event.target.result);
+      }
+      reader.readAsDataURL(image);
+      setIsBigger(false);
+      setIsUploaded(true);
+
+    } else {
+      setIsBigger(true);
+    }
+  }
+
+  const changeValue = (e) => {
+    const imageSize = e.target.files[0].size;
+    const image = e.target.files[0];
+
+    uploadImage(imageSize, image);
+  }
+
   return (
     <div className="upload-image-container">
       <label htmlFor="input-upload" className="upload-avatar">Upload Avatar</label>
       <div
-        className="upload-input-container"
+        className={`upload-input-container ${isDragged ? 'drag-over' : ''}`}
         tabIndex="0"
+        onDragOver={(event) => {
+          event.preventDefault();
+          setIsDragged(true);
+        }}
+
+        onDragLeave={(event) => {
+          event.preventDefault();
+          setIsDragged(false);
+        }}
+
+        onDrop={(event) => {
+          event.preventDefault();
+          const imageSize = event.dataTransfer.files[0].size;
+          const image = event.dataTransfer.files[0];
+
+          uploadImage(imageSize, image);
+          setIsDragged(false);
+        }}
       >
         <div className="upload-icon-container" onPointerUp={uploadFile}>
           <img ref={uploadedImage} src={UploadIcon} alt="upload icon" />
@@ -29,38 +71,20 @@ export function UploadArea({ setImage }) {
           accept=".jpg,.png"
           className="input-upload"
           ref={inputElem}
-          onChange={(e) => {
-            const imageSize = e.target.files[0].size;
-
-            if (imageSize / 1024 <= 500) {
-              const image = e.target.files[0];
-              const reader = new FileReader();
-              reader.onload = (event) => {
-                uploadedImage.current.src = event.target.result;
-                setImage(event.target.result);
-              }
-              reader.readAsDataURL(image);
-              setIsBigger(false);
-              setIsUploaded(true);
-
-            } else {
-              setIsBigger(true);
-            }
-
-          }}
+          onChange={changeValue}
         />
         <div className="drag-text">
-          { !isUploaded ? 'Drag and drop or click to upload' : ''}
-          { 
+          {!isUploaded ? 'Drag and drop or click to upload' : ''}
+          {
             isUploaded ?
-            <>
-              <button className="remove-btn btn" onPointerUp={() => {
-                uploadedImage.current.src = UploadIcon;
-                setIsUploaded(false);
-              }} >Remove image</button>
-              <button className="change-btn btn" onPointerUp={uploadFile}>Change image</button>
-            </> :
-            ''
+              <>
+                <button className="remove-btn btn" onPointerUp={() => {
+                  uploadedImage.current.src = UploadIcon;
+                  setIsUploaded(false);
+                }} >Remove image</button>
+                <button className="change-btn btn" onPointerUp={uploadFile}>Change image</button>
+              </> :
+              ''
           }
         </div>
       </div>
